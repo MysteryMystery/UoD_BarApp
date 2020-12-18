9ad65e3e63ff2db8da5ce0302e2c17e1b8adb063
James Attfield <jamesattfield1990@gmail.com>
2020-12-18 22:02:22 +0000

    m

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-#docker-compose up
+docker-compose up
 
-docker build ./REST_API -t rails:latest
+#docker build ./REST_API -t rails:latest
 
-docker run -p3000:3000 -v /c/users/distsys/desktop/rest:/app rails:latest
+#docker run -p3000:3000 -v /c/users/distsys/desktop/rest:/app rails:latest
