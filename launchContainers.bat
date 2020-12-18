d0cbc723d0622e178febfbb4705ebb969c57027e
James Attfield <jamesattfield1990@gmail.com>
2020-12-18 16:00:04 +0000

    re

--- launchContainers.bat
+++ launchContainers.bat
@@ -1,3 +1,9 @@
+cd FrontEnd
+ember build
+cd ..
+
 docker build ./REST_API -t rails:final
+docker build ./FrontEnd -t ember:final
 
 docker run --rm -d -p 3000:3000 rails:final
+docker run -dit --name apche-ember -p 8080:80 ember:final
